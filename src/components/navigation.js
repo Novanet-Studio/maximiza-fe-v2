b9b06e98b75ede2206b76663a255fe9f53012794
Freddy Polania <fpolania@gmail.com>
2022-03-07 16:57:38 -0400

    Navigation component update

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,5 @@
-import React from "react"
-import LinkNav from "./linkNav"
+import React from "react";
+import LinkNav from "./linkNav";
 
 const navigation = () => (
   <nav id="myNav" className="nav nav--overlay">
@@ -9,7 +9,12 @@ const navigation = () => (
         <p className="nav__texto">Nosotros</p>
         <div className="nav__dropdown">
           <LinkNav estilo="nav__link" text="Empresa" to="/empresa" />
-          <LinkNav estilo="nav__link" text="Servicios" to="/servicios" direction="left" />
+          <LinkNav
+            estilo="nav__link"
+            text="Servicios"
+            to="/servicios"
+            direction="left"
+          />
           <LinkNav estilo="nav__link" text="Directiva" to="/directiva" />
           <LinkNav estilo="nav__link" text="Contacto" to="/contacto" />
         </div>
@@ -19,8 +24,17 @@ const navigation = () => (
         <p className="nav__texto">Impacto ESG</p>
         <div className="nav__dropdown">
           <LinkNav estilo="nav__link" text="Private equity" to="/capital" />
-          <LinkNav estilo="nav__link" text="Educación" to="/educacion" direction="left" />
-          <LinkNav estilo="nav__link" text="Responsabilidad" to="/responsabilidad" />
+          <LinkNav
+            estilo="nav__link"
+            text="Educación"
+            to="/educacion"
+            direction="left"
+          />
+          <LinkNav
+            estilo="nav__link"
+            text="Responsabilidad"
+            to="/responsabilidad"
+          />
         </div>
       </div>
       <div className="nav__grupo">
@@ -30,10 +44,11 @@ const navigation = () => (
           <LinkNav estilo="nav__link" text="Medios" to="/medios" />
           <LinkNav estilo="nav__link" text="Blog" to="/blog" direction="left" />
           <LinkNav estilo="nav__link" text="Cumplimiento" to="/cumplimiento" />
+          <LinkNav estilo="nav__link" text="Sugerencias" to="/sugerencias" />
         </div>
       </div>
     </div>
   </nav>
-)
+);
 
-export default navigation
+export default navigation;
