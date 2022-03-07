957326f1d878fafe11ae2a5fc5c63c3e15e46c6d
Freddy Polania <fpolania@gmail.com>
2022-03-07 17:14:50 -0400

    Form method update

--- src/pages/sugerencias.js
+++ src/pages/sugerencias.js
@@ -92,7 +92,6 @@ const Sugerencias = ({ data }) => {
           <form
             className="datos__form"
             name="sugerencias"
-            method="POST"
             enctype="multipart/form-data"
             data-netlify="true"
             action="/gracias/"
